ccd601b92aa46b467adea4f87b10e2a9e27bb66f
MiguelLm84 <72008078+MiguelLm84@users.noreply.github.com>
2022-07-02 02:13:59 +0200

    version 2.0

--- app/src/main/java/com/miguel_lm/memorygame/model/provider/StaticVariables.kt
+++ app/src/main/java/com/miguel_lm/memorygame/model/provider/StaticVariables.kt
@@ -5,9 +5,16 @@ import com.miguel_lm.memorygame.R
 class StaticVariables {
 
     companion object {
+
+        //Constant for difficulty level selection
         const val EXTRA_SELECTION = "selection"
         const val BACKGROUND_ROW = R.drawable.fondo_logo
 
+        //VARIABLES FOR SHARED PREFERENCES
+        const val PREFERENCES_SPINNER = "preferences_spinner"
+        const val ITEM_POSITION = "item_position"
+
+        //List of Pokemon images
         const val PIKACHU = R.drawable.pikachu
         const val EEVEE = R.drawable.eevee
         const val CHARMELEON = R.drawable.charmeleon
@@ -23,6 +30,23 @@ class StaticVariables {
         const val PIDGEOT = R.drawable.pidgeot
         const val PATRAT = R.drawable.patrat
         const val DRAGONAIR = R.drawable.dragonair
+
+        //List of Simpson images
+        const val BART = R.drawable.bart
+        const val HOMER = R.drawable.homer
+        const val LISA = R.drawable.lisa
+        const val MARGE = R.drawable.marge
+        const val MAGGIE = R.drawable.maggie
+        const val NED_FLANDERS = R.drawable.ned_flander
+        const val OTTO = R.drawable.otto
+        const val WIGGUM = R.drawable.wiggum
+        const val MR_BARMS = R.drawable.mr_barms
+        const val SMITHERS = R.drawable.smithers
+        const val SKINNER = R.drawable.skinner
+        const val ABRAHAM = R.drawable.abraham
+        const val RALPH = R.drawable.ralph
+        const val KRUSTY = R.drawable.krusty
+        const val NELSON = R.drawable.milhouse
     }
 
 }
